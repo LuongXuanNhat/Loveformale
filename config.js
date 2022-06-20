8135b356fb1044d5278022d18e854a5e5ee2458d
LuongXuanNhat <xuannhat1932002@gmail.com>
2022-06-20 16:13:58 +0700

    Upload First

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    introTitle: 'Em ơi ~',
+    introDesc: `Những lời sau đây anh bày tỏ là những lời mà anh đã cất giấu trong tim này bấy lâu. 
+                Anh hãy trả lời thật lòng nhé :3`,
+    btnIntro: 'Hihi okii',
+    title: 'Giá như anh là nhà thơ anh sẽ tặng riêng em những vần thơ cháy bỏng. Nếu được là nhạc sĩ, anh sẽ viết tặng em những bản tình ca bất tận. Nếu là một lập trình viên anh sẽ viết nên những đoạn code ngập tràn hương vị của tình yêu', 
+    desc: 'Và giá như anh có thể anh sẽ làm tất cả vì em. Làm người yêu em nhé <3 ! Babe ❤️',
+    ket:'(Nếu không trả lời mà thoát ra là anh yêu em đấy nhá :3)',
+    btnYes: 'Đồng ý luôn <3',
+    btnNo: 'Không yêu :3',
+    question:'Thật tuyệt ! Khi cả hai ta cùng thích nhau. Em rất muốn biết tại sao anh thích em ?',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Em cũng yêu anh ạ',
+    mess: 'Em biết mà 🥰 Em yêu anh nhiều lắm 😘😘',
+    //mess:':3 Hihi',
+    //messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc:'Vậy còn ngần ngại gì nữa 🥰 Trò chuyện cùng anh thôi nào, gét gô',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.messenger.com/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    //messLink: 'http://fb.com'
+}
